b53dd6b7762524d46e9ae126f7fe0e107c229ef2
RekGRpth <rekgrpth@gmail.com>
2019-07-07 21:04:28 +0500

    up

--- pg_wkhtmltopdf.c
+++ pg_wkhtmltopdf.c
@@ -8,16 +8,16 @@
 
 PG_MODULE_MAGIC;
 
-static void progress_changed_callback(wkhtmltopdf_converter *converter, int p) {
+/*static void progress_changed_callback(wkhtmltopdf_converter *converter, int p) {
     printf("progress_changed_callback: %3d%%\n", p);
     fflush(stdout);
-}
+}*/
 
-static void phase_changed_callback(wkhtmltopdf_converter *converter) {
+/*static void phase_changed_callback(wkhtmltopdf_converter *converter) {
     int phase = wkhtmltopdf_current_phase(converter);
     printf("phase_changed_callback: %s\n", wkhtmltopdf_phase_description(converter, phase));
     fflush(stdout);
-}
+}*/
 
 static void error_callback(wkhtmltopdf_converter *converter, const char *msg) {
     fprintf(stderr, "error_callback: %s\n", msg);
@@ -29,10 +29,10 @@ static void warning_callback(wkhtmltopdf_converter *converter, const char *msg) 
     fflush(stderr);
 }
 
-static void finished_callback(wkhtmltopdf_converter *converter, int p) {
+/*static void finished_callback(wkhtmltopdf_converter *converter, int p) {
     printf("finished_callback: %3d%%\n", p);
     fflush(stdout);
-}
+}*/
 
 EXTENSION(wkhtmltopdf) {
     char *url;
@@ -48,11 +48,11 @@ EXTENSION(wkhtmltopdf) {
     if (!(object_settings = wkhtmltopdf_create_object_settings())) ereport(ERROR, (errmsg("!object_settings")));
     (void)wkhtmltopdf_set_object_setting(object_settings, "page", (const char *)url);
     if (!(converter = wkhtmltopdf_create_converter(global_settings))) ereport(ERROR, (errmsg("!converter")));
-    (void)wkhtmltopdf_set_progress_changed_callback(converter, progress_changed_callback);
-    (void)wkhtmltopdf_set_phase_changed_callback(converter, phase_changed_callback);
+//    (void)wkhtmltopdf_set_progress_changed_callback(converter, progress_changed_callback);
+//    (void)wkhtmltopdf_set_phase_changed_callback(converter, phase_changed_callback);
     (void)wkhtmltopdf_set_error_callback(converter, error_callback);
     (void)wkhtmltopdf_set_warning_callback(converter, warning_callback);
-    (void)wkhtmltopdf_set_finished_callback(converter, finished_callback);
+//    (void)wkhtmltopdf_set_finished_callback(converter, finished_callback);
     (void)wkhtmltopdf_add_object(converter, object_settings, NULL);
     if (!wkhtmltopdf_convert(converter)) ereport(ERROR, (errmsg("!wkhtmltopdf_convert")));
     if (!(len = wkhtmltopdf_get_output(converter, &data))) ereport(ERROR, (errmsg("!len")));
